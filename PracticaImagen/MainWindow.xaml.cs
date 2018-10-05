9acd2a9ca5869ba351ab910b2044914b87a9588f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-05 07:55:35 -0700

    Agregar archivos de proyecto.

---
+++ PracticaImagen/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaImagen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btncambImagen_Click(object sender, RoutedEventArgs e)
+        {
+            imgFennel.Source = new BitmapImage(
+                new Uri(@"C:\Users\alumno\Desktop\shirany_ax_by_elemen48-d9ge0f1.png"));
+        }
+    }
+}
